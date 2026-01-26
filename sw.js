269b713750861982e4c1c75fa9533d04c842e908
omri578 <omri.tablet2016@gmail.com>
2026-01-26 19:09:45 +0200

    Update service worker cache version to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "score-app-v1";
+const CACHE = "score-app-v2";
 const FILES = [
   "./",
   "index.html",
@@ -25,3 +25,4 @@ self.addEventListener("fetch", e => {
     caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
+
